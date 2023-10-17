a256a7114e9f676d338068fb08539f3e827e718c
Sagar Panchmatia <sagar.panchmatia@paysense.in>
2023-10-17 10:52:11 +0530

    UCIC updation event

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='crema',
-    version='2.33',
+    version='2.34',
     description='Kafka Library',
     url='https://github.com/paysense/crema',
     author='Rohit Laddha',
